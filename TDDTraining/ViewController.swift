b466849636dd7b75df4325c57b4262e187512ac8
weicheng <809405366@qq.com>
2020-01-14 14:09:13 +0800

    init

---
+++ TDDTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDDTraining
+//
+//  Created by Weicheng Wang on 2020/1/14.
+//  Copyright © 2020 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
